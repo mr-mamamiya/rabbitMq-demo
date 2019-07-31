4a4410ced1c52f32f9f92f3bdd24ad72fbb08bc0
Jiamiao.x <xiejiamiao0326@gmail.com>
2019-07-31 18:02:45 +0800

    add helloworld demo

---
+++ Jiamiao.x.RabbitMq.HelloWorld.Receive/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Jiamiao.x.RabbitMq.HelloWorld.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== RECEIVE ==========");
+
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("---------- RECEIVE MESSAGE ----------");
+                Console.WriteLine($"${message}");
+                Console.WriteLine("-------------------------------------");
+            };
+
+            channel.BasicConsume("hello", true, consumer);
+
+            Console.WriteLine("========== FINISH ==========");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Jiamiao.x.RabbitMq.HelloWorld.Send/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Jiamiao.x.RabbitMq.HelloWorld.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== SEND ==========");
+
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            var message = "Hello world";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+            Console.WriteLine($"---------- Send -> hello -> {message} ----------");
+
+            Console.WriteLine("========== FINISH ==========");
+            Console.ReadKey();
+        }
+    }
+}
