8c2d8877e47b45bf7620c0429e25f6e4ea8eac67
Jiamiao.x <xiejiamiao0326@gmail.com>
2019-08-01 11:48:09 +0800

    add work queues demo

---
+++ Jiamiao.x.RabbitMq.WorkQueues.Receive/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Jiamiao.x.RabbitMq.WorkQueues.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var receiverName = args[0];
+            Console.WriteLine($"========== WorkQueues.Receive-{receiverName} ==========");
+
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("task_queue", true, false, false, null);
+
+            channel.BasicQos(0, 1, false);
+
+            Console.WriteLine("Waiting for message...");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine("---------- RECEIVE MESSAGE ----------");
+                Console.WriteLine($"${message}");
+                Console.WriteLine("-------------------------------------");
+
+                Thread.Sleep(5000);
+
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
+
+            channel.BasicConsume("task_queue", false, consumer);
+            Console.WriteLine("Press anyKey to exit");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Jiamiao.x.RabbitMq.WorkQueues.Send/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace Jiamiao.x.RabbitMq.WorkQueues.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== WorkQueues.Send ==========");
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare("task_queue", true, false, false, null);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var message = $"Send message -> {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+                var body = Encoding.UTF8.GetBytes(message);
+                var properties = channel.CreateBasicProperties();
+
+                properties.Persistent = true;
+                channel.BasicPublish("", "task_queue", properties, body);
+                Console.WriteLine($"[{message}]  --> Send");
+                Thread.Sleep(2000);
+            }
+            
+            Console.WriteLine("Press anyKey to exit");
+            Console.ReadKey();
+        }
+
+    }
+}
